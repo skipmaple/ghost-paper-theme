9033350b19da8e7c81a24c0dc082f06173d8743f
lihaifeng <lihaifeng@sailyond.com>
2025-03-26 20:44:20 +0800

    feat: update header and footer.

--- assets/js/menuOpen.js
+++ assets/js/menuOpen.js
@@ -1,6 +1,6 @@
 // Toggle the menu open and close when on mobile
 export default function menuOpen() {
-    const burgerButton = document.querySelector('.gh-burger');
+    const burgerButton = document.querySelector('.gh-menu-button');
     burgerButton.addEventListener('click', function () {
         document.body.classList.toggle('gh-head-open');
     });
